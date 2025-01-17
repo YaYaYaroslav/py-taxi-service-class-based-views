79a5b5b2f3bb847243361a72a49c50a265a78e4e
Ярослав  Полінкін <yaroslav.ua.polinkin@gmail.com>
2025-01-17 10:17:55 +0200

    solution

--- taxi/urls.py
+++ taxi/urls.py
@@ -7,3 +7,26 @@ urlpatterns = [
 ]
 
 app_name = "taxi"
+from django.urls import path
+
+from .views import (
+    index,
+    ManufacturerListView,
+    CarListView,
+    CarDetailView,
+    DriverListView,
+    DriverDetailView,
+)
+
+urlpatterns = [
+    path("", index, name="index"),
+    path("manufacturers/", ManufacturerListView.as_view(),
+         name="manufacturer-list"),
+    path("cars/", CarListView.as_view(), name="car-list"),
+    path("cars/<int:pk>/", CarDetailView.as_view(), name="car-detail"),
+    path("drivers/", DriverListView.as_view(), name="driver-list"),
+    path("drivers/<int:pk>/", DriverDetailView.as_view(),
+         name="driver-detail"),
+]
+
+app_name = "taxi"
--- taxi/views.py
+++ taxi/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-
-from taxi.models import Driver, Car, Manufacturer
+from django.views.generic import ListView, DetailView
+from .models import Manufacturer, Car, Driver
 
 
 def index(request):
@@ -13,3 +13,29 @@ def index(request):
     }
 
     return render(request, "taxi/index.html", context=context)
+
+
+class ManufacturerListView(ListView):
+    model = Manufacturer
+    queryset = Manufacturer.objects.order_by("name")
+    paginate_by = 5
+
+
+class CarListView(ListView):
+    model = Car
+    queryset = Car.objects.select_related("manufacturer").order_by("id")
+    paginate_by = 5
+
+
+class CarDetailView(DetailView):
+    model = Car
+
+
+class DriverListView(ListView):
+    model = Driver
+    paginate_by = 5
+
+
+class DriverDetailView(DetailView):
+    model = Driver
+    queryset = Driver.objects.prefetch_related("cars")
